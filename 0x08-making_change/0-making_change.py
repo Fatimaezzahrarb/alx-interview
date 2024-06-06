6d87c5b18bafe163cb05d62a4b163646ba4f18b0
Fatimaezzahrarb <132583542+Fatimaezzahrarb@users.noreply.github.com>
2024-06-06 21:04:18 +0100

    0x08-making_change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    
+    sum = 0
+    if (total <= 0):
+        return 0
+    coins.sort(reverse=True)
+    for i in coins:
+        if (total < i):
+            pass
+        q, r = divmod(total, i)
+        total = r
+        sum += q
+    if (total != 0):
+        return -1
+    return sum
