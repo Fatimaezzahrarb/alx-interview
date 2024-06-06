18bfdd807a0bc654bd22d4ef5fed3ecbb6e436e5
Fatimaezzahrarb <132583542+Fatimaezzahrarb@users.noreply.github.com>
2024-06-06 21:08:40 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
+""" Contains the makeChange function"""
+
+
 def makeChange(coins, total):
-    
-    sum = 0
-    if (total <= 0):
-        return 0
-    coins.sort(reverse=True)
-    for i in coins:
-        if (total < i):
-            pass
-        q, r = divmod(total, i)
-        total = r
-        sum += q
-    if (total != 0):
+    """
+    Returns: fewest number of the coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
         return -1
-    return sum
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
